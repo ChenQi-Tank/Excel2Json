033829539de811c9f6ac80abd53e1d0b745e1583
ChenQi-Tank <13546116@qq.com>
2017-09-08 09:06:31 +0800

    Excel2Json

---
+++ Excel2Json/Excel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Data;
+using System.Data.OleDb;
+/// <summary>
+/// Excel 的摘要说明
+/// </summary>
+namespace Excel2Json
+{
+    public class Excel
+    {
+
+        public static DataSet SelectFromXLS(string file, string sheet)
+        {
+            string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + file + ";Extended Properties='Excel 8.0;IMEX=1;HDR=Yes'"; ;
+            OleDbConnection conn = new OleDbConnection(connStr);
+            OleDbDataAdapter da = null;
+            DataSet ds = new DataSet();
+            try
+            {
+                conn.Open();
+                da = new OleDbDataAdapter("select * from [" + sheet + "]", conn);
+                da.Fill(ds, "SelectResult");
+            }
+            catch (Exception e)
+            {
+                conn.Close();
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return ds;
+        }
+
+        public static DataTable GetXlsSheetName(string file)
+        {
+            string connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + file + ";Extended Properties='Excel 8.0;IMEX=1;HDR=No'"; ;
+            OleDbConnection conn = new OleDbConnection(connStr);
+            //OleDbDataAdapter da = null;
+            DataTable ExcelSheets = new DataTable();
+            try
+            {
+                conn.Open();
+                ExcelSheets = conn.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+            }
+            catch (Exception e)
+            {
+                conn.Close();
+                throw e;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return ExcelSheets;
+        }
+
+
+
+        public static string ConvertToSQLSheetName(string SheetName)
+        {
+            return "[" + SheetName + "]";
+        }
+    }
+}
---
+++ Excel2Json/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excel2Json
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel文件|*.xls;*.xlsx";
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = ofd.FileName;
+                DataTable dt = Excel.GetXlsSheetName(ofd.FileName);
+                if (dt != null)
+                {
+                    foreach(DataRow dr in dt.Rows)
+                    {
+                        comboBox1.Items.Add(dr[2]);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Excel文件格式异常");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (System.IO.File.Exists(path))
+            {
+               DataSet ds = Excel.SelectFromXLS(path, comboBox1.Text);
+               if (ds != null && ds.Tables != null && ds.Tables.Count > 0)
+               {
+                   Transform(ds.Tables[0]);
+               }
+               else
+                   MessageBox.Show("Excel文件中指定工作表格式异常");
+
+            }
+            else
+                MessageBox.Show("Excel文件不存在");
+        }
+
+        void Transform(DataTable dt)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+            foreach (DataRow dr in dt.Rows)
+            {
+                sb.Append("{");
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    sb.Append("\"");
+                    sb.Append(dt.Columns[i].ColumnName);
+                    sb.Append("\":");
+                    sb.Append("\"");
+                    sb.Append(dr[i].ToString().Replace("\n",""));
+                    sb.Append("\",");
+                }
+                sb.Remove(sb.Length - 1, 1);
+                sb.Append("},");
+            }
+            sb.Remove(sb.Length - 1, 1);
+            sb.Append("]");
+            richTextBox1.Text = sb.ToString();
+        }
+    }
+}
